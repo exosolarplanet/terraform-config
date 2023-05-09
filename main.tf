e4c4d474c1f92010adab18218b8f39155b049aca
Ece Deniz <ecedenniz@gmail.com>
2023-05-09 20:37:34 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -24,27 +24,10 @@ resource "google_sql_database_instance" "main" {
   settings {
     tier = "db-f1-micro"
     
-    database_flags {
-      name  = "cloudsql.iam_authentication"
-      value = "on"
-    }
   }
 }
 
-data "google_app_engine_default_service_account" "default" {
-}
-
-resource "google_project_iam_binding" "project" {
-  project = var.project_id
-  role    = "roles/cloudsql.instanceUser"
-
-  members = [
-    "serviceAccount:data.google_app_engine_default_service_account.default.email",
-  ]
-}
-
-resource "google_sql_user" "users" {
-  name     = data.google_app_engine_default_service_account.default.email
+resource "google_sql_database" "database" {
+  name     = "bornin-database"
   instance = google_sql_database_instance.main.name
-  type     = "CLOUD_IAM_USER"
 }
