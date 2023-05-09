b9683c02c8ff2f1ff5e354ace3437c4209ac2c12
Ece Deniz <ecedenniz@gmail.com>
2023-05-09 19:08:16 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,6 +9,8 @@ terraform {
 
 provider "google" {
     project = var.project_id
+    impersonate_service_account = "tf-bornin-project-sa@bornin-project.iam.gserviceaccount.com"
+
 }
 
 resource "google_app_engine_application" "app" {
