8d993cb9831ba971b1e9e55f552f7ae69a358162
exosolarplanet <ecedenniz@gmail.com>
2023-05-10 22:43:39 +0100

    fix CIDR range

--- main.tf
+++ main.tf
@@ -84,7 +84,7 @@ resource "google_vpc_access_connector" "connector" {
   provider = google-beta
 
   name          = "quickstart-connector"
-  ip_cidr_range = "10.8.0.0"
+  ip_cidr_range = "10.8.0.0/28"
   network       = google_compute_network.private_network.id
   region        = "europe-west3"
 }
