0e5dedca91a9ee238b235cb6a5a1eb60cd7f7446
exosolarplanet <ecedenniz@gmail.com>
2023-05-10 22:22:39 +0100

    add provider

--- main.tf
+++ main.tf
@@ -81,6 +81,8 @@ resource "google_sql_user" "users" {
 }
 
 resource "google_vpc_access_connector" "connector" {
+  provider = google-beta
+
   name          = "quickstart-connector"
   ip_cidr_range = "10.8.0.0"
   network       = google_compute_network.private_network.id
