43ab3059dbfa5963dd653039aea4960729adcc32
exosolarplanet <ecedenniz@gmail.com>
2023-05-10 22:10:05 +0100

    add vpc access connector

--- main.tf
+++ main.tf
@@ -80,9 +80,9 @@ resource "google_sql_user" "users" {
   
 }
 
-# resource "google_vpc_access_connector" "connector" {
-#   name          = "quickstart-connector"
-#   ip_cidr_range = "10.8.0.0/28"
-#   network       = "default"
-# }
+resource "google_vpc_access_connector" "connector" {
+  name          = "quickstart-connector"
+  ip_cidr_range = "10.8.0.0/28"
+  network       = "default"
+}
 
